b7f84408a1cd541ed75237a6a279366e701e55f3
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2025-02-04 17:15:03 +0000

    js added

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+const check_btn = document.getElementById("check-button");
+const results = document.getElementById("result");
+let isPalindrome;
+check_btn.addEventListener("click", () => {
+  const textInput = document.getElementById("text-input").value.trim();
 
+  if (!textInput) {
+    alert("Please input a value");
+    return;
+  }
 
-const styles = 'padding: 20px; background-color: yellow; color: green';
+  const processedText = textInput.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
-console.log('%Hello world', styles);
+  if (processedText === processedText.split('').reverse().join('')) {
+    isPalindrome = true;
+    results.innerHTML = `${textInput} is a Palindrome`
+
+  } else {
+    isPalindrome = false;
+    results.innerHTML = `"${textInput}" is not a Palindrome`
+
+  }
+
+});
